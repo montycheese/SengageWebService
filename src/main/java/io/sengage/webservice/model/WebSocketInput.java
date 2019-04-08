1c918e90dd5247e4f2616f50614c25a46ac9d2e5
Montana <montanawong@gmail.com>
2019-04-07 20:52:50 -0700

    Add lambda type and fix template typos

---
+++ src/main/java/io/sengage/webservice/model/WebSocketInput.java
@@ -0,0 +1,44 @@
+package io.sengage.webservice.model;
+
+public class WebSocketInput {
+
+
+	private RequestContext requestContext;
+	private String body;
+	private boolean isBase64Encoded;
+	 
+	public static class RequestContext {
+		private String routeKey;
+		private String authorizer;
+		private String messageId;
+		private String integrationLatency;
+	    private String eventType;
+		private String error;
+	    private String extendedRequestId;
+		private String requestTime;
+		private String messageDirection;
+		private String stage;
+		private float connectedAt;
+		private float requestTimeEpoch;
+		private Identity identity;
+		private String requestId;
+		private String domainName;
+		private String connectionId;
+		private String apiId;
+		private String status;
+		 
+		public static class Identity {
+			private String cognitoIdentityPoolId = null;
+			private String accountId = null;
+			private String cognitoIdentityId = null;
+			private String caller = null;
+			private String sourceIp;
+			private String accessKey = null;
+			private String cognitoAuthenticationType = null;
+			private String cognitoAuthenticationProvider = null;
+			private String userArn = null;
+			private String userAgent = null;
+			private String user = null;
+		}
+	}
+}
