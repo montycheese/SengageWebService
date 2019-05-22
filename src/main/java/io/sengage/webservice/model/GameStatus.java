ef8f1f98815b14ac39369a18300e182f46db0279
Montana <montanawong@gmail.com>
2019-05-22 00:16:48 -0700

    Add ordinals to game status

--- src/main/java/io/sengage/webservice/model/GameStatus.java
+++ src/main/java/io/sengage/webservice/model/GameStatus.java
@@ -1,10 +1,37 @@
 package io.sengage.webservice.model;
 
+import lombok.Getter;
+
 public enum GameStatus {
-	INIT,
-	WAITING_FOR_PLAYERS,
-	IN_PROGRESS,
-	COMPLETED,
-	TERMINAL,
-	ERROR_STATE
+	INIT(0),
+	WAITING_FOR_PLAYERS(1),
+	IN_PROGRESS(2),
+	COMPLETED(3),
+	TERMINAL(4),
+	ERROR_STATE(5)
+	;
+	
+	@Getter
+	private final int ordinal;
+	
+	private GameStatus(int ordinal) {
+		this.ordinal = ordinal;
+	}
+	
+	public boolean isOnOrBefore(GameStatus other) {
+		return this.ordinal <= other.ordinal;
+	}
+	
+	public boolean isBefore(GameStatus other) {
+		return this.ordinal < other.ordinal;
+	}
+	
+	public boolean isOnOrAfter(GameStatus other) {
+		return this.ordinal >= other.ordinal;
+	}
+	
+	public boolean isAfter(GameStatus other) {
+		return this.ordinal > other.ordinal;
+	}
+	
 }
