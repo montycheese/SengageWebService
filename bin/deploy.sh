13c10628753cc4995c8e7ce6828138457bc995d4
Montana <montanawong@gmail.com>
2019-06-19 08:20:14 -0700

    Add script to deploy pkg

---
+++ bin/deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -z "$1" ]
+  then
+    echo "Argument stage is required"
+    exit 1
+fi
+
+STAGE=$1
+
+echo "Build maven project..."
+
+mvn clean
+
+mvn compile
+
+mvn package
+
+sam package --template-file template.yaml --s3-bucket deployment-us-east-1-lambdas-1 --output-template-file packaged.yaml
+
+sam deploy --template-file ./packaged.yaml --stack-name sengage-webservice --capabilities CAPABILITY_IAM --parameter-overrides DeploymentStage=${STAGE}
